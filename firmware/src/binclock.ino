710faea03745afe9f612cb1f41c2e94d74f6842c
Gergely VAKULYA <vakulya@dcs.uni-pannon.hu>
2019-04-26 21:34:19 +0200

    Basic functionality, no time setting.

--- firmware/src/binclock.ino
+++ firmware/src/binclock.ino
@@ -1,7 +1,7 @@
 #define LED_CLK 1
 #define LED_DATA 0
 
-#define LEDS 3
+#define LEDS 17
 
 typedef struct
 {
@@ -65,6 +65,125 @@ void updateLeds()
   delay(1);
 }
 
+volatile uint32_t lastPress=0;
+volatile uint8_t lastButtonState=1;
+volatile uint32_t pressedMicros;
+uint32_t lastPrint=0;
+volatile uint8_t hours=0, minutes=0, secs=0;
+volatile uint8_t ch=0;
+
+void printTime();
+
+void incHours()
+{
+  hours++;
+  if (hours>=24)
+  {
+    hours=0;
+  }
+}
+
+void incMinutes()
+{
+  minutes++;
+  if (minutes>=60)
+  {
+    minutes=0;
+    incHours();
+  }
+}
+
+void incSecs()
+{
+  secs++;
+  if (secs>=60)
+  {
+    secs=0;
+    incMinutes();
+  }
+}
+
+void newColor(uint8_t idx)
+{
+  if (!(leds[idx].r | leds[idx].g | leds[idx].b))
+  {
+    uint8_t a, b;
+    a=rand() % 3;
+    b=rand() % 10;
+    if (a==0)
+    {
+      leds[idx].r=b;
+      leds[idx].g=10-b;
+      leds[idx].b=0;
+    }
+    else if (a==1)
+    {
+      leds[idx].r=0;
+      leds[idx].g=b;
+      leds[idx].b=10-b;
+    }
+    else
+    {
+      leds[idx].r=10-b;
+      leds[idx].g=0;
+      leds[idx].b=b;
+    }
+  }
+}
+
+void printTime()
+{
+  int8_t i;
+  uint8_t idx;
+  uint8_t b;
+  for (i=0; i<=4; i++)
+  {
+    idx=4-i;
+    b=(hours >> i) & 1;
+    if (b)
+    {
+      newColor(idx);
+    }
+    else
+    {
+      leds[idx].r=0;
+      leds[idx].g=0;
+      leds[idx].b=0;
+    }
+  }
+  for (i=0; i<=5; i++)
+  {
+    idx=i+5;
+    b=(minutes >> i) & 1;
+    if (b)
+    {
+      newColor(idx);
+    }
+    else
+    {
+      leds[idx].r=0;
+      leds[idx].g=0;
+      leds[idx].b=0;
+    }
+  }
+  for (i=0; i<=5; i++)
+  {
+    idx=16-i;
+    b=(secs >> i) & 1;
+    if (b)
+    {
+      newColor(idx);
+    }
+    else
+    {
+      leds[idx].r=0;
+      leds[idx].g=0;
+      leds[idx].b=0;
+    }
+  }
+  updateLeds();
+}
+
 void setup()
 {
   pinMode(LED_CLK, OUTPUT);
@@ -72,18 +191,31 @@ void setup()
   digitalWrite(LED_DATA, 0);
   digitalWrite(LED_CLK, 0);
 
-  leds[0].r=20; leds[0].g= 0; leds[0].b= 0;
-  leds[1].r=10; leds[1].g=10; leds[1].b=10;
-  leds[2].r= 0; leds[2].g=20; leds[2].b= 0;
+//  leds[ 0].r=20; leds[ 0].g= 0; leds[ 0].b= 0;
+//  leds[ 1].r=10; leds[ 1].g=10; leds[ 1].b=10;
+//  leds[ 2].r= 0; leds[ 2].g=20; leds[ 2].b= 0;
+//  leds[ 3].r=20; leds[ 3].g= 0; leds[ 3].b= 0;
+//  leds[ 4].r=10; leds[ 4].g=10; leds[ 4].b=10;
+//
+//  leds[ 5].r= 0; leds[ 5].g=20; leds[ 5].b= 0;
+//  leds[ 6].r=20; leds[ 6].g= 0; leds[ 6].b= 0;
+//  leds[ 7].r=10; leds[ 7].g=10; leds[ 7].b=10;
+//  leds[ 8].r= 0; leds[ 8].g=20; leds[ 8].b= 0;
+//  leds[ 9].r=20; leds[ 9].g= 0; leds[ 9].b= 0;
+//  leds[10].r=10; leds[10].g=10; leds[10].b=10;
+//
+//  leds[11].r= 0; leds[11].g=20; leds[11].b= 0;
+//  leds[12].r=20; leds[12].g= 0; leds[12].b= 0;
+//  leds[13].r=10; leds[13].g=10; leds[13].b=10;
+//  leds[14].r= 0; leds[14].g=20; leds[14].b= 0;
+//  leds[15].r=20; leds[15].g= 0; leds[15].b= 0;
+//  leds[16].r=10; leds[16].g=10; leds[16].b=10;
   delay(10);
 }
 
 void loop()
 {
-  while(1)
-  {
-    updateLeds();
-    delay(3);
-  }
-  while(1);
+  printTime();
+  delay(1000);
+  incSecs();
 }
